336a73febf2908436bb18444d0ed4816f2ce9e12
damian_pileckas <damianpileckas@itrsa.com.ar>
2020-06-20 19:23:48 -0300

    conexionBD

--- src/server/app.js
+++ src/server/app.js
@@ -1,30 +1,41 @@
 import express from 'express'
-import { getUsuariosRouter } from './routers/usuariosRouter.js'
-
+//import getUsuariosRouter from './routers/usuariosRouter.js'
+import mysql from 'mysql';
+import getUsuariosRouter from "./routers/getUsuariosRouter.js";
 class App {
 
+
     constructor() {
-        const app = express()
-        app.use(express.json())
-        app.set('json spaces', 4)
-        app.use('/api/usuarios', getUsuariosRouter())
-        this.app = app
+        const app = express();
+        app.use(express.json());
+
+        app.set('json spaces', 4);
+        app.use('/api/usuarios', getUsuariosRouter());
+        app.use(mysql.createConnection({
+            host: '127.3.3.1',
+            user: 'root',
+            password: '',
+            port: 3306,
+            database: 'gestionpermisos'
+        }));
+        this.app = app;
+
     }
 
     setOnReady(cb) {
-        this.app.on('app_ready', cb)
+        this.app.on('app_ready', cb);
     }
 
     async start(port) {
         if (!port) {
-            port = 0
+            port = 0;
         }
 
         const server = this.app.listen(port, () => {
-            const actualPort = server.address().port
-            this.app.emit("app_ready", actualPort)
-        })
+            const actualPort = server.address().port;
+            this.app.emit("app_ready", actualPort);
+        });
     }
 }
 
-export default App
+export { App };
---
+++ src/server/routers/getUsuariosRouter.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import UsuariosApi from '../apis/UsuariosApi.js';
+
+function getUsuariosRouter() {
+
+    const router = express.Router();
+    const usuariosApi = new UsuariosApi();
+
+    //LISTADO DE USUARIOS - ENDPOINT http://localhost:8080/api/usuarios?email=perez@perez.com&habilitado=SI
+    router.get('/', async(req, res) => {
+        try {
+            const queryParams = new Map(Object.entries(req.query));
+            const usuarios = await usuariosApi.buscar(queryParams);
+            res.json(usuarios);
+        } catch (err) {
+            res.status(err.estado).json(err);
+        }
+    });
+
+    //AGREGAR USUARIOS - ENDPOINT http://localhost:8080/api/usuarios
+    router.post('/', async(req, res) => {
+        const usuParaAgregar = req.body;
+
+        try {
+            const usuAgregado = await usuariosApi.agregar(usuParaAgregar);
+            res.status(201).json(usuAgregado);
+        } catch (err) {
+            res.status(err.estado).json(err);
+        }
+    });
+
+    //ELIMINAR USUARIOS - ENDPOINT http://localhost:8080/api/usuarios
+    router.delete('/:id', async(req, res) => {
+        try {
+            await usuariosApi.borrar(req.params.id);
+            res.status(204).send();
+        } catch (err) {
+            res.status(err.estado).json(err);
+        }
+    });
+
+    //MODIFICAR USUARIOS - ENDPOINT http://localhost:8080/api/usuarios
+    router.put('/:id', async(req, res) => {
+        const usuParaReemplazar = req.body;
+
+        try {
+            const usuReemplazado = await usuariosApi.reemplazar(req.params.id, usuParaReemplazar);
+            res.json(usuReemplazado);
+        } catch (err) {
+            res.status(err.estado).json(err);
+        }
+    });
+
+    return router;
+}
